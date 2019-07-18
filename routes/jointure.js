45c51bc8c22ff03157bf6983dfec6836cabbef34
b3b3r <b3b3rseg@gmail.com>
2019-07-18 23:07:12 +0200

    ajout route filter

--- routes/jointure.js
+++ routes/jointure.js
@@ -11,45 +11,84 @@ router.use(bodyParser.urlencoded({
 }));
 
 router.get('/theme/circus', (req, res) => {
-  connection.query(
-    `SELECT circus.name, circus.price, circus.place, circus.url, theme.name AS theme
-    FROM circus 
+  const { theme } = req.query;
+  if (theme) {
+    connection.query(
+      `SELECT circus.name, circus.price, circus.place, circus.url, theme.name AS theme
+    FROM circus
     JOIN theme_circus ON circus.id = theme_circus.id_circus
-    JOIN theme ON theme_circus.id_theme= theme.id`,
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Erreur lors de l\'import de données');
-      } else {
-        const newResults = results;
-        const finalData = [];
-        const filter = [...new Set(newResults.map(result => result.name))];
+    JOIN theme ON theme_circus.id_theme= theme.id
+    WHERE theme.name=?`, theme,
+      (err, results) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Erreur lors de l\'import de données');
+        } else {
+          console.log(results);
+
+          const newResults = results;
+          const finalData = [];
+          const filter = [...new Set(newResults.map(result => result.name))];
 
 
-        for (let index = 0; index < filter.length; index++) {
-          const newData = [...new Set(newResults.filter(data => data.name === filter[index])
-            .map(result => result.theme))];
-          const name = filter[index];
-          const { price, place, url } = newResults.find(x => x.name === filter[index]);
-          finalData.push({
-            name,
-            price,
-            place,
-            url,
-            theme: newData
-          });
+          for (let index = 0; index < filter.length; index++) {
+            const newData = [...new Set(newResults.filter(data => data.name === filter[index])
+              .map(result => result.theme))];
+            const name = filter[index];
+            const { price, place, url } = newResults.find(x => x.name === filter[index]);
+            finalData.push({
+              name,
+              price,
+              place,
+              url,
+              theme: newData
+            });
+          }
+
+          res.json(finalData);
         }
-
-        res.json(finalData);
       }
-    }
-  );
+    )
+  }
+  else {
+    connection.query(
+      `SELECT circus.name, circus.price, circus.place, circus.url, theme.name AS theme
+      FROM circus 
+      JOIN theme_circus ON circus.id = theme_circus.id_circus
+      JOIN theme ON theme_circus.id_theme= theme.id`,
+      (err, results) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Erreur lors de l\'import de données');
+        } else {
+          const newResults = results;
+          const finalData = [];
+          const filter = [...new Set(newResults.map(result => result.name))];
+
+
+          for (let index = 0; index < filter.length; index++) {
+            const newData = [...new Set(newResults.filter(data => data.name === filter[index])
+              .map(result => result.theme))];
+            const name = filter[index];
+            const { price, place, url } = newResults.find(x => x.name === filter[index]);
+            finalData.push({
+              name,
+              price,
+              place,
+              url,
+              theme: newData
+            });
+          }
+
+          res.json(finalData);
+        }
+      }
+    )
+  };
 });
 
 router.post('/theme/circus', (req, res) => {
   const formData = req.body;
-  console.log(formData);
-
   connection.query('INSERT INTO theme_circus SET ?', formData, (err) => {
     if (err) {
       console.log(err);
